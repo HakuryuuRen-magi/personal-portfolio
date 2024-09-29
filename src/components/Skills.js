df62f86f9fb8e91c644a54f40e236d194f004e72
HakuryuuRen-magi <michaelkurniawan65@gmail.com>
2024-09-29 14:11:39 +0700

    Added more Carousel slide on Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -36,7 +36,11 @@ export const Skills = () => {
             <div className="skill-bx wow zoomIn">
               <h2>Skills</h2>
               <p>
-              With a strong foundation in Web Development and Machine Learning/Deep Learning, I’ve applied my technical expertise across various hands-on projects. Whether building responsive websites or crafting intelligent models, I continuously push the boundaries of innovation.
+                With a strong foundation in Web Development and Machine
+                Learning/Deep Learning, I’ve applied my technical expertise
+                across various hands-on projects. Whether building responsive
+                websites or crafting intelligent models, I continuously push the
+                boundaries of innovation.
               </p>
               <Carousel
                 responsive={responsive}
@@ -48,16 +52,32 @@ export const Skills = () => {
                   <h5>Web Development</h5>
                 </div>
                 <div className="item">
-                  <img src={meter3} alt="Image" />
-                  <h5>Embedded Systems</h5>
+                  <img src={meter1} alt="Image" />
+                  <h5>Machine Learning</h5>
                 </div>
                 <div className="item">
-                  <img src={meter2} alt="Image" />
-                  <h5>Machine/Deep Learning</h5>
+                  <img src={meter1} alt="Image" />
+                  <h5>Deep Learning</h5>
                 </div>
                 <div className="item">
                   <img src={meter3} alt="Image" />
-                  <h5>React Language</h5>
+                  <h5>Data Analytics</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="Image" />
+                  <h5>CI/CD Pipelines (Jenkins, GitLab CI)</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="Image" />
+                  <h5>Big Data Technologies</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="Image" />
+                  <h5>Cloud Platforms (AWS, GCP, Azure)</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="Image" />
+                  <h5>React.js</h5>
                 </div>
                 <div className="item">
                   <img src={meter1} alt="Image" />
@@ -68,27 +88,43 @@ export const Skills = () => {
                   <h5>Python</h5>
                 </div>
                 <div className="item">
-                  <img src={meter2} alt="Image" />
-                  <h5>Machine/Deep Learning</h5>
+                  <img src={meter3} alt="Image" />
+                  <h5>SQL</h5>
                 </div>
                 <div className="item">
-                  <img src={meter3} alt="Image" />
-                  <h5>Data Analytic</h5>
+                  <img src={meter2} alt="Image" />
+                  <h5>Node.js</h5>
                 </div>
                 <div className="item">
                   <img src={meter1} alt="Image" />
-                  <h5>Problem Solving</h5>
+                  <h5>Django</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="Image" />
+                  <h5>Express.js</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="Image" />
+                  <h5>RESTful APIs</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="Image" />
+                  <h5>GraphQL</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="Image" />
+                  <h5>JWT Authentication</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="Image" />
+                  <h5>Microservices Architecture</h5>
                 </div>
                 <div className="item">
                   <img src={meter3} alt="Image" />
                   <h5>Team Work</h5>
                 </div>
                 <div className="item">
-                  <img src={meter1} alt="Image" />
-                  <h5>Hard Work</h5>
-                </div>
-                <div className="item">
-                  <img src={meter3} alt="Image" />
+                  <img src={meter2} alt="Image" />
                   <h5>Leadership</h5>
                 </div>
                 <div className="item">
